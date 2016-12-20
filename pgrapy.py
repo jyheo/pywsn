1dfb116e8eb09c5f5c974cf6ac885da0c68f20e8
Junyoung Heo <jyheo0@gmail.com>
2016-12-21 02:14:41 +0900

    functions instead of class with methods

--- pgrapy.py
+++ pgrapy.py
@@ -1,28 +1,40 @@
+# -*- coding: utf-8 -*-
 import networkx as nx
 from math import sqrt
 
-class PGraph(nx.Graph):
-    def __init__(self):
-        super(self.__class__, self).__init__()
-        self.pos = {}
 
-    def add_node_with_pos(self, n, x, y, **kwargs):
-        super(self.__class__, self).add_node(n, kwargs)
-        self.node[n]['pos'] = (x, y)
-        self.pos[n] = (x, y)
+def get_pos_dict(graph):
+    pos = {}
+    for n in graph.nodes_iter():
+        pos[n] = graph.node[n]['pos']
+    return pos
 
-    def remove_node(self, n):
-        super(self.__class__, self).remove_node(n)
-        del self.pos[n]
 
-    def distance(self, n1, n2):
-        pos1 = self.pos[n1]
-        pos2 = self.pos[n2]
-        return sqrt(pow(pos1[0] - pos2[0], 2) + pow(pos1[1] - pos2[1], 2))
+def add_node_with_pos(graph, n, x, y, **kwargs):
+    graph.add_node(n, kwargs)
+    graph.node[n]['pos'] = (x, y)
 
-    def center_pos(self, node_set):
-        pos_x = [self.node[n]['pos'][0] for n in node_set]
-        pos_y = [self.node[n]['pos'][1] for n in node_set]
-        c_x = int((min(pos_x) + max(pos_x)) / 2)
-        c_y = int((min(pos_y) + max(pos_y)) / 2)
-        return c_x, c_y
+
+def distance(graph, n1, n2):
+    pos1 = graph.node[n1]['pos']
+    pos2 = graph.node[n2]['pos']
+    return sqrt(pow(pos1[0] - pos2[0], 2) + pow(pos1[1] - pos2[1], 2))
+
+
+def center_pos(graph):
+    pos_x = [graph.node[n]['pos'][0] for n in graph.nodes_iter()]
+    pos_y = [graph.node[n]['pos'][1] for n in graph.nodes_iter()]
+    c_x = int((min(pos_x) + max(pos_x)) / 2)
+    c_y = int((min(pos_y) + max(pos_y)) / 2)
+    return c_x, c_y
+
+
+def nodes_covered_by_circle(graph, x, y, radius):
+    covered_nodes = []
+    for n in graph.nodes_iter():
+        pos_x = graph.node[n]['pos'][0]
+        pos_y = graph.node[n]['pos'][1]
+        rsq = (x - pos_x) * (x - pos_x) + (y - pos_y) * (y - pos_y)
+        if rsq <= radius * radius:
+            covered_nodes.append(n)
+    return covered_nodes

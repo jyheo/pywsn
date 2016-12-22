fcfc995b943079c1d307db3ef56b0247f8326829
Junyoung Heo <jyheo0@gmail.com>
2016-12-23 00:25:07 +0900

    draw plot

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 
 import random
 import networkx as nx
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import pgrapy as pg
 import wsn
@@ -17,31 +18,7 @@ def make_nodes_randomly(width, height, num_of_nodes):
     return nodes
 
 
-def print_stat(monitoring_area):
-    rn = len(monitoring_area.redundant_nodes())
-    uc = sum([n.number_of_nodes() for n in monitoring_area.uncovered_area()])
-    print 'redundant nodes: ', rn, 'uncovered_ipoints: ', uc
-
-
-if __name__ == '__main__':
-    prop = wsn.Properties()
-    prop.wireless_range = 30
-    prop.sensing_range = 15
-    prop.width = 100
-    prop.height = 100
-    prop.grid_size = 3
-
-    nodes = make_nodes_randomly(prop.width, prop.height, 33)
-
-    MA = wsn.MonitoringArea(prop)
-    for n in nodes:
-        MA.add_node(n[0], n[1], n[2])
-
-    print_stat(MA)
-
-    MA.draw_area()
-    plt.show()
-
+def move_redundant_nodes_to_uncovered_area(MA):
     rn = list(MA.redundant_nodes())
     uncovered_area_graphs = MA.uncovered_area()
     uncovered_area_graphs = sorted(uncovered_area_graphs, key=len, reverse=True)
@@ -60,7 +37,89 @@ if __name__ == '__main__':
         uncovered_area_graphs.extend(sub)
         uncovered_area_graphs = sorted(uncovered_area_graphs, key=len, reverse=True)
 
-    print_stat(MA)
 
-    MA.draw_area()
+def print_stat(monitoring_area):
+    rn = len(monitoring_area.redundant_nodes())
+    uc = sum([n.number_of_nodes() for n in monitoring_area.uncovered_area()])
+    print 'sensing range: ', monitoring_area.wsn_prop.sensing_range,
+    print 'redundant nodes: ', rn, 'uncovered_ipoints: ', uc, uc*100/monitoring_area.ipoints.number_of_nodes()
+
+
+if __name__ == '__main__':
+    prop = wsn.Properties()
+    prop.wireless_range = 30
+    prop.sensing_range = 15
+    prop.width = 100
+    prop.height = 100
+    prop.grid_size = 5
+
+    nodes = make_nodes_randomly(prop.width, prop.height, 23)
+
+    # EXP1, variable grid_size, sensing_range
+    # for gs in range(10, 0, -1):
+    #     print gs,
+    # print
+    #
+    # for sr in range(5, 25):
+    #     prop.sensing_range = sr
+    #     # EXP1: for granularity of the grid_size
+    #     print sr,
+    #     for gs in range(10, 0, -1):
+    #         prop.grid_size = gs
+    #         MA = wsn.MonitoringArea(prop)
+    #         for n in nodes:
+    #             MA.add_node(n[0], n[1], n[2])
+    #         #print_stat(MA)
+    #         uc = sum([n.number_of_nodes() for n in MA.uncovered_area()])
+    #         uc = uc*100/MA.ipoints.number_of_nodes()
+    #         print uc,
+    #     print
+
+    # EXP2, variable grid_size, variable nodes
+    prop.sensing_range = 15
+    x_pos = []
+    y_pos = []
+    uc_list = []
+    for nn in range(20, 50, 5):
+        nodes = make_nodes_randomly(prop.width, prop.height, nn)
+        print nn,
+        for gs in range(10, 0, -1):
+            prop.grid_size = gs
+            MA = wsn.MonitoringArea(prop)
+            for n in nodes:
+                MA.add_node(n[0], n[1], n[2])
+            # print_stat(MA)
+            uc = sum([n.number_of_nodes() for n in MA.uncovered_area()])
+            uc = uc * 100 / MA.ipoints.number_of_nodes()
+            x_pos.append(nn)
+            y_pos.append(gs)
+            uc_list.append(uc)
+            print uc,
+        print
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.bar3d(x_pos, y_pos, [0]*len(x_pos), 1, 0.5, uc_list, color='b')
     plt.show()
+
+
+    # MA = wsn.MonitoringArea(prop)
+    # for n in nodes:
+    #     MA.add_node(n[0], n[1], n[2])
+    #
+    # print_stat(MA)
+    #
+    # #MA.draw_area()
+    # #plt.show()
+    #
+    # move_redundant_nodes_to_uncovered_area(MA)
+    #
+    # print_stat(MA)
+    #
+    # #MA.draw_area()
+    # #plt.show()
+    #
+    # MA2 = wsn.MonitoringArea(prop)
+    # for n in nodes:
+    #     MA2.add_node(n[0], n[1], n[2])
+    # print_stat(MA2)
